c0de19ec9e9c65e88ee26e8362292fc734b4c193
zainaDjaoud <zynadj92@gmail.com>
2022-10-08 19:14:55 -0400

    firt commit

---
+++ src/java_basic_assignement/My_First_Assignement.java
@@ -0,0 +1,53 @@
+package java_basic_assignement;
+
+public class My_First_Assignement {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(20);
+		System.out.println(100);
+		System.out.println("hello");
+		System.out.println("Hi");
+		System.out.println("good morning");
+		System.out.println("thank you");
+		System.out.println("a very good session");
+		System.out.println("i understand a lot");
+		System.out.println("Good job");
+		System.out.println("work");
+		System.out.println("my name");
+		System.out.println("last name");
+		System.out.println("user name");
+		System.out.println("password");
+		System.out.println("True");
+		System.out.println("false");
+		System.out.println("nice");
+		System.out.println("work smart");
+		System.out.println("helpful");
+		
+		// Ststment without ln
+		System.out.print(1);
+		System.out.print(20);
+		System.out.print(100);
+		System.out.print("hello");
+		System.out.print("Hi");
+		System.out.print("good morning");
+		System.out.print("thank you");
+		System.out.print("a very good session");
+		System.out.print("i understand a lot");
+		System.out.print("Good job");
+		System.out.print("work");
+		System.out.print("my name");
+		System.out.print("last name");
+		System.out.print("user name");
+		System.out.print("password");
+		System.out.print("True");
+		System.out.print("false");
+		System.out.print("nice");
+		System.out.print("work smart");
+		System.out.print("helpful");
+		
+		
+
+	}
+
+}
---
+++ src/java_basic_assignement/My_First_Assignement_1.java
@@ -0,0 +1,102 @@
+package java_basic_assignement;
+
+public class My_First_Assignement_1 {
+
+	public static void main(String[] args) {
+		
+		// int
+		int a = 10;
+		int b = 20;
+		int c = 1150;
+		int d = 36987;
+		int k = -125875;
+		int h = -9874125;
+		int v = 589741;
+		int s = -5;
+		int t = -88;
+		int n = 2019;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(k);
+		System.out.println(h);
+		System.out.println(v);
+		System.out.println(s);
+		System.out.println(t);
+		System.out.println(n);
+
+		// boolean
+		boolean a1= 5<9;
+		boolean q= 100>56;
+		boolean r= 88884>10.99999;
+		boolean p= 56549>9999545;
+		boolean i2= 300<9.6699;
+		boolean z= 555>556;
+		boolean x= 85213649> 45687;
+		boolean y1= 1.3654>-555;
+		boolean f4= -5<-6665;
+		boolean L= 400<95647;
+		
+		System.out.println(a1);
+		System.out.println(q);
+		System.out.println(r);
+		System.out.println(p);
+		System.out.println(i2);
+		System.out.println(z);
+		System.out.println(x);
+		System.out.println(y1);
+		System.out.println(f4);
+		System.out.println(L);
+		
+		//Char
+		char u = 'M';
+		char u1 = '<';
+		char u2 = '*';
+		char u3 = '@';
+		char u4 = '%';
+		char u5 = '5';
+		char u6 = '&';
+		char G = '#';
+		char G1 = '9';
+		char G2 = '!';
+		
+		System.out.println(u);
+		System.out.println(u1);
+		System.out.println(u2);
+		System.out.println(u3);
+		System.out.println(u4);
+		System.out.println(u5);
+		System.out.println(u6);
+		System.out.println(G);
+		System.out.println(G1);
+		System.out.println(G2);
+		
+		 // double
+		double e =14.12;
+		double e1 = 10.23;
+		double e2 = 5555.01;
+		double e3 = 0.000001;
+		double e4 = 9999999.999;
+		double e5 = 6.4453698751234;
+		double e6 = 80.808465;
+		double e7 = 1111111.2;
+		double e8 = 96541238745.258741369;
+		double e9 = 3264587.25;
+		double e10 = 44045.75555;
+		
+		System.out.println(e);
+		System.out.println(e1);
+		System.out.println(e2);
+		System.out.println(e3);
+		System.out.println(e4);
+		System.out.println(e5);
+		System.out.println(e6);
+		System.out.println(e7);
+		System.out.println(e8);
+		System.out.println(e9);
+		System.out.println(e10);
+	}
+
+}
