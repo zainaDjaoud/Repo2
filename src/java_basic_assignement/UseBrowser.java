e0d3d483132cdadf75ed67340714d148d0ef1be2
zainaDjaoud <zynadj92@gmail.com>
2022-10-08 20:07:18 -0400

    adding use Browser class

---
+++ src/java_basic_assignement/UseBrowser.java
@@ -0,0 +1,12 @@
+package java_basic_assignement;
+
+public class UseBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n=1;
+System.out.println("n");
+
+	}
+
+}
